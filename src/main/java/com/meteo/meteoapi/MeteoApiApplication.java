304bef7b22402c41526d60ebe5fc91c4024e28c0
Sérgio Gasalho <paulogasalho@ua.pt>
2022-10-28 23:42:00 +0100

    #1 Initialize Spring project

---
+++ src/main/java/com/meteo/meteoapi/MeteoApiApplication.java
@@ -0,0 +1,13 @@
+package com.meteo.meteoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meteo/meteoapi/MeteoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteo.meteoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
